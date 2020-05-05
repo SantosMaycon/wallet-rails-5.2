1001152928169d2913fa10b8880feb1a1308d781
SantosMaycon <mayconalves.santos14@gmail.com>
2020-05-05 01:06:27 +0000

    Trabalhando com helper, hash para parâmetro de url e rails generate controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def data_br(data_us)
+        data_us.strftime("%d/%m/%Y")
+    end
+    
+    def ambiente_rails
+        if Rails.env.production?
+            "Produção"
+        elsif Rails.env.development?
+            "Desenvolvimento"
+        else
+            "Teste"
+        end
+    end
 end
